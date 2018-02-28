08ba02cd0161ce1ffe5980965da8f610d0ec91ee
sunhongjian <sunhj@huntor.cn>
2018-02-28 16:15:58 +0800

    新增将数据添加到mongodb

--- config/default.js
+++ config/default.js
@@ -1,4 +1,5 @@
 module.exports = {
+	mongoUrl: 'mongodb://localhost:27017/coupons',
 	urlIndex: 'http://s.hadefile.com/index/44612',
 	lowPrice: 'http://s.hadefile.com/search?kw=&cate=&sorts=&price_range%5B%5D=0&price_range%5B%5D=20&sales_range%5B%5D=2000&page=',
 	jquery: 'https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js'
--- crawler.js
+++ crawler.js
@@ -1,12 +1,13 @@
 const phantom = require('phantom');
 const config = require('./config/default');
 const fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
 
 let allData = [];
 const crawler = {
   MAX_PAGE: 5,
   implement: async (pageNum) => {
-    var URL = config.lowPrice+pageNum;
+    var URL = config.lowPrice + pageNum;
     var instance = await phantom.create();
     var page = await instance.createPage();
     var status = await page.open(URL);
@@ -40,24 +41,20 @@ const crawler = {
       })
     }
     await instance.exit();
-    if(result.dataLength > 0 && pageNum <= crawler.MAX_PAGE) {
-      // 将旧文件中的数据提取出来,把新数据拼上重新储存到原文件
-      var oldFileString = fs.readFileSync('data/data.json').toString();
-      var fileData = '';
-      if(oldFileString) {
-        fileData = JSON.stringify(JSON.parse(oldFileString).concat(result.content));
-      } else {
-        fileData = JSON.stringify(result.content);
-      }
-      fs.writeFile('data/data.json', fileData , function (err) {
+    if (result.dataLength > 0 && pageNum <= crawler.MAX_PAGE) {
+      MongoClient.connect(config.mongoUrl, function (err, db) {
         if (err) throw err;
-        console.log('小丁便宜购第'+pageNum+'已被写入');
+        var dbo = db.db("coupons");
+        dbo.collection("site").insertMany(result.content, function (err, res) {
+          if (err) throw err;
+          console.log("插入的文档数量为: " + res.insertedCount);
+          db.close();
+        });
       });
-      allData.concat(result.content);
-      await crawler.implement(pageNum+1)
+      await crawler.implement(pageNum + 1)
     } else {
       return allData;
-    }  
+    }
   }
 }
 module.exports = {
