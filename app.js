729d11f330a713babec522ec927a1f1b7b969a96
sunhongjian <sunhj@huntor.cn>
2018-03-01 09:59:47 +0800

    新增 colors插件

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const Koa = require('koa');
 const app = new Koa();
 const crawler = require('./crawler');
+var colors = require('colors');  
+  
+  
+colors.setTheme({  
+    silly: 'rainbow',  
+    input: 'grey',  
+    verbose: 'cyan',  
+    prompt: 'red',  
+    info: 'green',  
+    data: 'blue',  
+    help: 'cyan',  
+    warn: 'yellow',  
+    debug: 'magenta',  
+    error: 'red'  
+});  
 
-console.log('服务已经启动');
+console.log('服务已经启动'.green);
 app.use(crawler.create);
 
 app.listen(3000);
